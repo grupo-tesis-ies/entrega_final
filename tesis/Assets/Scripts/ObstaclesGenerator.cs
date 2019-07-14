fd59c8c9b3132ced0f57b6bb998a5da6ff979b6d
Gonzalo Espeche <espeche.gonzalo@gmail.com>
2019-07-14 15:38:10 +0200

    initial setup (#1)

---
+++ tesis/Assets/Scripts/ObstaclesGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesGenerator : MonoBehaviour
+{
+    public GameObject[] obstacles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("InstantiateObs", 0.5f, 1f);
+    }
+
+    void InstantiateObs()
+    {
+        
+    }
+}
